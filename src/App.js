e3c163b7ece49b7c7f91941a4c265dbcbe13d293
AbolfazlKhosravi <abolfazlkhosravi.2003jun@gmail.com>
2023-02-09 10:58:26 +0330

    create Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Provider } from "react-redux";
+import Navbar from "./components/navbaeer";
 import store from "./components/redux/root";
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="">
+      <div className="h-screen  bg-gray-900">
+       <Navbar/>
       </div>
     </Provider>
   );
---
+++ src/components/navbaeer.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <header className="flex w-full justify-center items-center p-4 text-white font-bold  bg-gray-700">
+      <h1>Inventoru App using Redux & React </h1>
+      <div className="ml-2 bg-gray-600 w-5 h-5 flex justify-center items-center rounded-full ring-2 ring-white">
+        <span className="text-sm">7</span>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
